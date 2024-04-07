7e33935146f9285b10469458ae3c22c92e7e24b2
tanuj <tanujbhatt644@gmail.com>
2024-04-07 22:16:52 +0530

    worked on specfic game page

--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,18 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex justify-start items-center h-[80vh] pl-2">
-      <div className="w-[50%] flex flex-col gap-4">
+      <div className="w-full lg:w-[50%] flex flex-col gap-4">
         <h1 className="text-5xl font-bold my-2">
-          Free<span className="">Play</span>
+          Free<span className=""> Play</span>
         </h1>
-        <p className="text-muted-foreground">
-          Discover and Play Games Tailored Just for You
+        <p className="text-muted-foreground leading-8">
+          Discover and Play Games Tailored Just for You.{" "}
         </p>
         <p className="leading-8 text-muted-foreground">
           Looking for a place to indulge in your favorite games or explore new
-          ones? Look no further! [Your Platform Name] offers a diverse
-          collection of games across genres, ensuring there's something for
-          everyone.
+          ones? Look no further! <span className="text-primary">FreePlay</span>{" "}
+          offers a diverse collection of games across genres, ensuring there's
+          something for everyone.
         </p>
         <Link href="/games" className="mt-4">
           <Button>Get Started</Button>
